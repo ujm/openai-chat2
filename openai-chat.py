61e3357aee0fc969edae5fa11ae2bb9e5c98d38c
YoshiakiGOTO <yoshiaki_gotou@qdenbs.com>
2023-06-23 14:40:16 +0900

    first commit

--- openai-chat.py
+++ openai-chat.py
@@ -3,49 +3,44 @@ import os
 import time
 import pickle
 
-api_key = os.environ['OPENAI_API_KEY']
+MODEL_NAME = "gpt-3.5-turbo"
+SLEEP_INTERVAL = 5
+PICKLE_FILE = 'my_array.pickle'
+PROMPT = 'あなた: '
+CONTINUE_PROMPT = '>>>'
+ERROR_MESSAGE = '処理中...'
+EXIT_MESSAGE = '[終了するには "ctrl+c" と入力してください。]\n\n'
 
-openai.api_key = api_key
+print(EXIT_MESSAGE)
 
-print('[終了するには \"ctrl+c\" と入力してください。]\n\n')
 message = {"role":"user", "content":""}
-conversation = [{"role": "system", "content": "日本語"}]
+conversation = [{"role": "system", "content": ""}]
 
 while True:
     input_str = ""
+
     while True:
-        if input_str == "":
-            line = input('あなた: ')
-        else:
-            line = input('>>>')
+        line = input(PROMPT if input_str == "" else CONTINUE_PROMPT)
         if line == "":
             break
         input_str += line + "\n"
+
     # 最後の改行を削除
     input_str = input_str.rstrip('\n')
 
-    message = {"role":"user", "content":input_str};
+    message = {"role":"user", "content":input_str}
     conversation.append(message)
 
     while True:
         try:
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
+            completion = openai.ChatCompletion.create(model=MODEL_NAME, messages=conversation)
             break
-        except:
-            time.sleep(5)
-            print('処理中...')
-            continue
+        except openai.error.OpenAIError:
+            print(ERROR_MESSAGE)
+            time.sleep(SLEEP_INTERVAL)
 
-    if(len(conversation)==2):
-        with open('my_array.pickle', 'wb') as f:
-            pickle.dump(conversation, f)
+    with open(PICKLE_FILE, 'wb') as f:
+        pickle.dump(conversation, f)
 
-    print(len(conversation))
     print(f"アシスタント: {completion.choices[0].message.content}\n")
-    print('-------------------- total_tokens を表示--------------------')
-    print(completion.usage.total_tokens)
-
-    if(completion.usage.total_tokens == 4097) :
-        del conversation[0]
-        
     conversation.append(completion.choices[0].message)
--- openai-whisper.py
+++ openai-whisper.py
@@ -2,19 +2,44 @@ import openai
 import sys
 import os
 
-api_key = os.environ['OPENAI_API_KEY']
+def transcribe_audio(sound_path):
+    # Open the audio file
+    with open(sound_path, "rb") as audio_file:
+        # Transcribe the audio file
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-openai.api_key = api_key
+    # Format the transcript
+    formatted_transcript = transcript["text"].replace("。", "。\n")
+    print(f"Transcription:\n{formatted_transcript}")
 
-sound_path = sys.argv[1]
+    return transcript["text"]
 
-audio_file= open(sound_path, "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print(transcript["text"]+"\n")
-translation = transcript["text"]+"を日本語に訳して。"
+def chat_with_gpt(initial_message):
+    conversation = [{"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": initial_message}]
 
-conversation = [{"role": "system", "content": "日本語翻訳"}]
-conversation.append({"role": "user", "content": translation})
+    while True:
+        user_input = input("Your question: ")
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
-print(completion.choices[0].message.content.replace("。", "。\n"))
+        if user_input.lower() == "exit":
+            print("Ending the chat.")
+            break
+
+        conversation.append({"role": "user", "content": user_input})
+
+        # Limit conversation history to last 4 user messages
+        conversation = conversation[-4:]
+
+        # Get response from GPT-3.5-turbo model
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation
+        )
+
+        # Print the summarized content
+        print(f"GPT-3.5-turbo response:\n{completion.choices[0].message.content}\n")
+
+if __name__ == "__main__":
+    sound_path = sys.argv[1]
+    initial_transcription = transcribe_audio(sound_path)
+    chat_with_gpt(initial_transcription)
